155bb0ec2be27c4a90dcb5333156a522f3c744a4
100QNKOLEV <shadoowkrit1@gmail.com>
2021-05-12 17:06:50 +0300

    added util.js

---
+++ BanichkaApp/util.js
@@ -0,0 +1,23 @@
+import moment from 'moment';
+import { interpolate } from 'react-native-reanimated';
+
+export function formatDate(dateString) {
+    const parsedDate = moment(new Date(dateString));
+
+    if(!parsedDate.isValid()) { //parsedDate.isValid() == false
+        return Date.now();
+    }
+
+    return parsedDate.format('DD/MM/YYYY HH:mm');
+}
+
+export function getCountdownParts(eventDate) {
+    const duration = moment.duration(moment(new Date(eventDate))
+        .diff(new Date()));
+    return {
+        days: parseInt(duration.as('days')),
+        hours: duration.get('hours'),
+        minutes: duration.get('minutes'),
+        seconds: duration.get('seconds')
+    }
+}
